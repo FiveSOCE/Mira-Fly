51ca3ae5ce1502e7e95020946a85adc574560f56
FiveS <sgmmsionide@gmail.com>
2026-09-03 23:31:13 +1000

    Register MiraFly faction flight authority

--- src/main/java/com/mira/fly/MiraFlyPlugin.java
+++ src/main/java/com/mira/fly/MiraFlyPlugin.java
@@ -1,21 +1,27 @@
 package com.mira.fly;
 
+import com.mira.factions.api.FactionFlightController;
+import com.mira.factions.api.MiraFactionsApi;
 import com.mira.fly.command.FlyCommand;
 import com.mira.fly.listener.FlyVoucherListener;
 import com.mira.fly.listener.VoucherProtectionListener;
+import com.mira.fly.service.FactionFlightBridge;
 import com.mira.fly.service.FlyManager;
 import com.mira.fly.service.FlyTimeService;
 import com.mira.fly.service.FlyVoucherService;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MiraFlyPlugin extends JavaPlugin implements Listener {
     private FlyTimeService time;
     private FlyVoucherService vouchers;
     private FlyManager manager;
+    private FactionFlightBridge factionBridge;
 
     @Override
     public void onEnable() {
@@ -23,6 +29,18 @@ public final class MiraFlyPlugin extends JavaPlugin implements Listener {
         time = new FlyTimeService(this);
         vouchers = new FlyVoucherService(this);
         manager = new FlyManager(this, time);
+
+        if (getServer().getPluginManager().isPluginEnabled("MiraFactions")) {
+            MiraFactionsApi factions = getServer().getServicesManager().load(MiraFactionsApi.class);
+            if (factions != null) {
+                factionBridge = new FactionFlightBridge(factions, manager);
+                manager.setFactionAccess(factionBridge);
+                getServer().getServicesManager().register(FactionFlightController.class, factionBridge, this, ServicePriority.Normal);
+                getLogger().info("MiraFactions flight integration enabled. MiraFly owns runtime flight state.");
+            } else {
+                getLogger().warning("MiraFactions is enabled but its API service was not available; faction flight integration is disabled.");
+            }
+        }
 
         FlyCommand commands = new FlyCommand(this, manager, time, vouchers);
         registerCommand("fly", commands);
@@ -34,13 +52,20 @@ public final class MiraFlyPlugin extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         manager.start();
+        getServer().getOnlinePlayers().forEach(manager::refresh);
         getLogger().info("MiraFly v" + getPluginMeta().getVersion() + " enabled.");
     }
 
     @Override
     public void onDisable() {
+        getServer().getServicesManager().unregisterAll(this);
         if (manager != null) manager.shutdown();
         else if (time != null) time.save();
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (manager != null) getServer().getScheduler().runTask(this, () -> manager.refresh(event.getPlayer()));
     }
 
     @EventHandler
